ffac26117076614b19407f77be77d408c6afdbf8
David Fernandes <davidsf@qiteam.org>
2022-06-08 14:47:35 +0300

    docker done

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import dotenv
 import grpc
 import jsonpickle
 
+import json
+
 from gis.gis_pb2 import CreateNetworkInput, CreateNetworkOutput, OptimizeNetworkInput, OptimizeNetworkOutput
 from gis.gis_pb2_grpc import GISModuleServicer, add_GISModuleServicer_to_server
 from module.functions.create_network import run_create_network
@@ -18,7 +20,7 @@ dotenv.load_dotenv()
 class GISModule(GISModuleServicer):
 
   def __init__(self) -> None:
-      super().__init__()
+    pass
 
   def create_network(self, request: CreateNetworkInput, context) -> CreateNetworkOutput:
     input_dict = {
@@ -29,9 +31,9 @@ class GISModule(GISModuleServicer):
     output = run_create_network(input_data=input_dict, KB=KB(data=kb))
 
     return CreateNetworkOutput(
-      road_nw = jsonpickle.encode(output['road_nw'], unpicklable=True),
-      n_demand_dict = jsonpickle.encode(output['n_demand_dict'], unpicklable=True),
-      n_supply_dict = jsonpickle.encode(output['n_supply_dict'], unpicklable=True),
+      road_nw = json.dumps(output['road_nw']),
+      n_demand_dict = json.dumps(output['n_demand_dict']),
+      n_supply_dict = json.dumps(output['n_supply_dict']),
     )
 
   def optimize_network(self, request:OptimizeNetworkInput, context) -> OptimizeNetworkOutput:
@@ -44,24 +46,24 @@ class GISModule(GISModuleServicer):
     output = run_optimize_network(input_data=input_dict, KB=KB(data=kb))
 
     return OptimizeNetworkOutput(
-      res_sources_sinks = jsonpickle.encode(output['res_sources_sinks'], unpicklable=True),
-      sums = jsonpickle.encode(output['sums'], unpicklable=True),
-      losses_cost_kw = jsonpickle.encode(output['losses_cost_kw'], unpicklable=True),
-      network_solution_nodes = jsonpickle.encode(output['network_solution_nodes'], unpicklable=True),
-      network_solution_edges = jsonpickle.encode(output['network_solution_edges'], unpicklable=True),
-      potential_edges = jsonpickle.encode(output['potential_edges'], unpicklable=True),
-      potential_nodes = jsonpickle.encode(output['potential_nodes'], unpicklable=True),
-      selected_agents = jsonpickle.encode(output['selected_agents'], unpicklable=True),
+      res_sources_sinks = json.dumps(output['res_sources_sinks']),
+      sums = json.dumps(output['sums']),
+      losses_cost_kw = json.dumps(output['losses_cost_kw']),
+      network_solution_nodes = json.dumps(output['network_solution_nodes']),
+      network_solution_edges = json.dumps(output['network_solution_edges']),
+      potential_edges = json.dumps(output['potential_edges']),
+      potential_nodes = json.dumps(output['potential_nodes']),
+      selected_agents = json.dumps(output['selected_agents']),
     )
 
 
 def serve():
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  add_GISModuleServicer_to_server(GISModule, server)
+  add_GISModuleServicer_to_server(GISModule(), server)
 
   server.add_insecure_port(f"{os.getenv('GRPC_HOST')}:{os.getenv('GRPC_PORT')}")
 
-  print(f"Market module Listening at {os.getenv('GRPC_HOST')}:{os.getenv('GRPC_PORT')}")
+  print(f"GIS module Listening at {os.getenv('GRPC_HOST')}:{os.getenv('GRPC_PORT')}")
 
   server.start()
   server.wait_for_termination()
